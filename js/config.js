1b70710fdb8b0ed5cacfbbb509dc1a976f938d49
qamastermanagement-pixel <qamastermanagement@gmail.com>
2026-02-11 20:09:42 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,13 @@
-// Google Apps Script Web App URL
-const CONFIG = {
+// js/config.js
+// Pastikan Google Sheets  sudah "Publish to web" sebagai CSV
+
+window.CONFIG = {
+  // Google Apps Script Web App URL
   APPS_SCRIPT_URL:
     "https://script.google.com/macros/s/AKfycbytpHuYFDR_G-sugVMYFVpEbw1uQObHt68HiiRsuo01YybVLh_otjhjW971CO9QrH5gtA/exec",
-}
 
-// Make CONFIG available globally
-window.CONFIG = CONFIG
+  // Google Sheets CSV (publish)
+  // Kalau kamu mau pakai yang kamu kirim sebelumnya, paste di sini
+  MASTER_DATA_URL:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTrVEMf_DG702fbz5Gy12__YvNYc1lNXTW-gFcZbV5J0NSndYYvjQb_HmjsEWImsZBLAEZqlTs9eLDh/pub?gid=0&single=true&output=csv",
+};
